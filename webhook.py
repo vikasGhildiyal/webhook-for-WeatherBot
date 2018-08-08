7bc3aacdd08b7338e73d32f121514c85f550e8ae
vikasGhildiyal <41319365+vikasGhildiyal@users.noreply.github.com>
2018-08-09 00:27:41 +0530

    Update webhook.py

--- webhook.py
+++ webhook.py
@@ -27,16 +27,9 @@ result=req.get("result")
 parameters=result.get("parameters")
 city=parameters.get("geo-city")
 date=parameters.get("date")
-r=requests.get('api.openweathermap.org/data/2.5/forecast?q=London,us&appid=b6907d289e10d714a6e88b30761fae22')
-json_object=r.json()
-weather=json_object['list']
-for i in range(0,30):
- if date in weather[i]['dt_txt']:
-   condition=weather[i]['weather'][0]['description']
-   break
-   
+  
 
-speech="The forecast for"+city+ "for "+date+" is "+condition
+speech="The forecast for"+city+ "for "+date+" is very hot"
 return{
 "speech":speech,
 "displayText":speech,
