90555d8040a4b4612395f72a10a51b872083ae66
vikasGhildiyal <41319365+vikasGhildiyal@users.noreply.github.com>
2018-08-20 18:25:50 +0530

    Update webhook.py

--- webhook.py
+++ webhook.py
@@ -36,4 +36,4 @@ return{
 if __name__=='__main__':
 port=int(os.getenv('PORT',5000))
 print("Starting app on port %d" % port)
-app.run(debug=False,port=port,host='0.0.0.0')
+app.run(debug=False,port=int(os.environ['PORT']),host='0.0.0.0')
