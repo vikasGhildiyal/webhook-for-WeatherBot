25e3e9065c20a325eb1131932b3c88cc720327c9
vikasGhildiyal <41319365+vikasGhildiyal@users.noreply.github.com>
2018-08-21 23:20:50 +0530

    Update webhook.py

--- webhook.py
+++ webhook.py
@@ -8,26 +8,7 @@ from flask import make_response
 app=Flask(__name__)
 @app.route('/webhook',methods=['POST'])
 def webhook():
-req=request.get_json(silent=True,force=True)
-print(json.dumps(req,indent=4))
-res=makeResponse(req)
-res=json.dumps(req,indent=4)
-r=make_response(res)
-r.headers['Content-Type']='application/json'
-return r
-def makeResponse(req):
-result=req.get("result")
-parameters=result.get("parameters")
-city=parameters.get("geo-city")
-date=parameters.get("date")
-r=requests.get('http://api.openweathermap.org/data/2.5/forecast?q='+city+'&appid=b6907d289e10d714a6e88b30761fae22')
-json_object=r.json()
-weather=json_object['list']
-for i in range(0,30):
- if date in weather[i]['dt_txt']:
-   condition=weather[i]['weather'][0]['description']
-   break
-speech="The forecast for"+city+ "for "+date+" is "+condition
+speech="hot"
 return{
 "speech":speech,
 "displayText":speech,
